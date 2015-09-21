5774f2c0a1aca889872a1698be7f949d20b0571c
wguo <wei.guo@tekcomms.com>
2015-09-21 17:26:54 +0800

    Add first activity.

---
+++ app/src/main/java/com/applepyarc/family/bean/WaterDrop.java
@@ -0,0 +1,22 @@
+package com.applepyarc.family.bean;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by wguo on 9/21/2015.
+ */
+
+public class WaterDrop {
+
+    private UUID mID;
+    private String mName;
+    private String mGender;
+    private Date mBirth;
+    private Date mDead;
+    private UUID mParent;
+    private UUID mSpouse;
+    private UUID mChildren;
+    private String mDesc;
+
+}
